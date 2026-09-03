b022150179de4cc0e4ad121c3b727b1d33d72271
anastasiasasaki <anastasiasasaki1@gmail.com>
2026-09-03 09:41:20 +0900

    user input function added

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 
 i = 10
 r = 2
-d = 3
+d = int(input("Enter the rounds won: "))
 sum_score = 0
 score = i
 
 while d > 1:
-    score += r * i*(d-1)
+    score += r * i * (d - 1)
     sum_score += score
-    d = d - 1
+    d -= 1
 
 print("Total score:", score)
